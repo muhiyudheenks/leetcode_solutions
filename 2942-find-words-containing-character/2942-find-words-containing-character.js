03f1f119ebb6d18ebebc0f146b16282b42cc1f1f
muhiyadheenks <muhiyadheensj@gmail.com>
2026-07-02 14:54:21 +0530

    Time: 2 ms (38.63%), Space: 57.5 MB (72.33%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.js
+++ 2942-find-words-containing-character/2942-find-words-containing-character.js
@@ -6,14 +6,8 @@
 var findWordsContaining = function (words, x) {
     let result = [];
     for (let i = 0; i < words.length; i++) {
-        let word = words[i];
-
-        for (let j of word) {
-            if (j.includes(x)) {
-               result.push(i)
-               break;
-
-            }
+        if(words[i].includes(x)){
+            result.push(i)
         }
     }
     return result
